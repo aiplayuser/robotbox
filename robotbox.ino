5c81529d533b8cac7603000d8c5cac00f1afa51d
aiplayuser <25455926@qq.com>
2025-07-03 08:30:14 +0800

    updata imdex.html

--- robotbox.ino
+++ robotbox.ino
@@ -9,11 +9,12 @@
 #include <Servo.h>//舵机库
 #include <ArduinoJson.h>//json数据库
 #include <Ticker.h>//多任务定时器
-WiFiManager espwifi; ESP8266WebServer espweb(80); DNSServer dnsServer; Servo q,w,e,r,t,y,u,i; Ticker ticker; File file; int ms=100,ds=30; bool ting=0;
+WiFiManager espwifi; ESP8266WebServer espweb(80); DNSServer dnsServer; Servo q,w,e,r,t,y,u,i; Ticker ticker; File file; 
+int ms=100,ds=30; bool ting=0,ziyou=0;
 
 void setup(){q.attach(D1);w.attach(D2);e.attach(D5);r.attach(D6); t.attach(D7);y.attach(D8);u.attach(D9);i.attach(D10);
   q.write(90);w.write(90);e.write(90);r.write(90);t.write(90);y.write(90);u.write(90);i.write(90);
-  pinMode(D0, INPUT); pinMode(D3, INPUT_PULLUP); pinMode(D4, OUTPUT); digitalWrite(D4,0);
+  pinMode(D0, INPUT); pinMode(D3, INPUT_PULLUP); pinMode(D4, OUTPUT); digitalWrite(D4,0); randomSeed(analogRead(A0));
   
   SPIFFS.begin();String ssid = "ESP"+String(ESP.getChipId(),HEX);
   if(SPIFFS.exists("/pass.json")){DynamicJsonDocument doc(50); file = SPIFFS.open("/pass.json", "r");deserializeJson(doc, file); 
@@ -41,7 +42,8 @@ void cmdsave(){ //修改json命令
     String mingzi = espweb.arg("mingzi"); int dong = espweb.arg("dongzuo").toInt(); int buint = espweb.arg("bushu").toInt();
     DynamicJsonDocument doc(8000); file = SPIFFS.open("/config.json", "r"); deserializeJson(doc, file);
     if(mingzi=="删除"){buint==0 ? doc.remove(dong) : doc[dong].remove(buint);}
-    else if(mingzi=="随机训练"){randomSeed(analogRead(A0)); int ran[9]={0,30,45,60,90,120,135,150,180}; doc[dong][0]["name"]=mingzi;
+    else if(mingzi=="自由活动"){delay(999); ziyou=!ziyou;cmddanbu();}
+    else if(mingzi=="随机训练"){int ran[9]={0,30,45,60,90,120,135,150,180}; doc[dong][0]["name"]=mingzi;
        for(int i=1;i<=buint;i++){int qv,wv,ev,rv,tv,yv,uv,iv;
           wv=random(181);ev=random(181);yv=random(181);uv=random(181);
           wv>90?qv=random(0,91):qv=random(90,181); ev>90?rv=random(0,91):rv=random(90,181);
@@ -59,29 +61,31 @@ void handleA0(){ //读取json文件
       html+=j+10; html+=")><input  type=range name=dd id="; html+=j+10; html+=" max=200 oninput=d.value=this.value onchange=setpwm(";
       html+=j+10; html+=")><br>速度<input type=number style=width:45px name=s oninput=ss.value=this.value value=30 onchange=setpds(";
       html+=j+100; html+=")><input type=range name=ss id="; html+=j+100; html+=" max=200 oninput=s.value=this.value onchange=setpds(";
-      html+=j+100; html+=")><br></form>"; html+="<button onclick=sendbufa("; html+=j; html+=",0)>";
+      html+=j+100; html+=")><br></form>"; html+="<button style=background:#84C1FF;font-weight:bold onclick=sendbufa("; html+=j; html+=",0)>";
       html+=j; html+="："; html+=doc[j][0]["name"].as<String>();  html+="</button>速度控制<br>";
       for(int k=1;k<doc[j].size();k++){html+=k; html+="<button onclick=sendbufa("; html+=j; html+=","; html+=k; html+=")>";
         for(int i=0;i<8;i++){html+=se[i]+doc[j][k][se[i]].as<int>();}  html+="</button><br>";}}
     file.close(); espweb.send(200, "text/html", html);}
-void cmddanbu(){ting=!ting;//执行json命令
-        int sv[8]; String sn[8]{"q","w","e","r","t","y","u","i"};
-        int dong = espweb.arg("dong").toInt(); int bufa = espweb.arg("bufa").toInt();
-        DynamicJsonDocument doc(8000); file = SPIFFS.open("/config.json", "r"); deserializeJson(doc, file);
-    while(ting){for(int n=1; n<doc[dong].size(); n++){  
-        ceju(); float limi=pulseIn(D0,1)/58;
-        while(limi<20){if(ting==0)break; delaybu(99);
-             for(int m=0;m<3;m++){if(ting==0)break;for(int n=1; n<doc[3].size(); n++){
-                 if(ting==0)break;for(int i=0;i<8;i++)sv[i]=doc[3][n][sn[i]]; 
-                 danbu(sv[0],sv[1],sv[2],sv[3],sv[4],sv[5],sv[6],sv[7]);delaybu(ms);} } 
-             ceju(); limi=pulseIn(D0,1)/58; if(limi>20)delaybu(99);}
-        if(ting==0)break; int m=n; if(bufa!=0){m=bufa;n=doc[dong].size();}
-        for(int i=0;i<8;i++)sv[i]=doc[dong][m][sn[i]]; 
-        danbu(sv[0],sv[1],sv[2],sv[3],sv[4],sv[5],sv[6],sv[7]); 
-        if(bufa!=0)ting=0; delaybu(ms); }}
+void cmddanbu(){ting=!ting; if(ting==0)ziyou=0;//执行json命令
+    int sv[8]; String sn[8]{"q","w","e","r","t","y","u","i"};
+    int dong = espweb.arg("dong").toInt(); int bufa = espweb.arg("bufa").toInt();
+    DynamicJsonDocument doc(8000); file = SPIFFS.open("/config.json", "r"); deserializeJson(doc, file);
+    while(ting){int doint=dong; if(ziyou)doint=random(2,doc.size()); 
+      if(ceju()<20){jiange(5000);doint=4;}
+      for(int m=0;m<2;m++){
+         for(int n=1; n<doc[doint].size(); n++){if(ting==0)break;
+            //if(doc[doint][0]["name"]=="随机训练"){ms=random(100);ds=random(100);}
+            //if(ziyou){ms=random(100);ds=random(100);} 
+            if(ceju()<20&&doint!=4)break; 
+            int buint=n; if(bufa!=0)buint=bufa;
+            for(int i=0;i<8;i++)sv[i]=doc[doint][buint][sn[i]]; 
+            danbu(sv[0],sv[1],sv[2],sv[3],sv[4],sv[5],sv[6],sv[7]); 
+            if(bufa!=0)ting=0; jiange(ms*50); }}}
     file.close();}
-void ceju(){digitalWrite(D4,0); delayMicroseconds(5);digitalWrite(D4,1);delayMicroseconds(10); digitalWrite(D4,0);}
-void delaybu(int j){ for(int k=0;k<j;k++){espweb.handleClient();dnsServer.processNextRequest();delay(10);} }
+float ceju(){digitalWrite(D4,0); delayMicroseconds(5);digitalWrite(D4,1);delayMicroseconds(10); 
+             digitalWrite(D4,0); return pulseIn(D0,1)/58;}
+void jiange(int j){ for(int k=0;k<j*10;k++){espweb.handleClient();ArduinoOTA.handle();
+                                         dnsServer.processNextRequest();delayMicroseconds(10);} }
 void danbu(int q2,int w2,int e2,int r2,int t2,int y2,int u2,int i2){
    int q1=q.read(),w1=w.read(),e1=e.read(),r1=r.read(),t1=t.read(),y1=y.read(),u1=u.read(),i1=i.read();
    while(q1!=q2 || w1!=w2 || e1!=e2 || r1!=r2 || t1!=t2 || y1!=y2 || u1!=u2 || i1!=i2){if(ting==0)break;
@@ -89,7 +93,7 @@ void danbu(int q2,int w2,int e2,int r2,int t2,int y2,int u2,int i2){
         if(e1!=e2){e2>e1 ? e1++:e1--;} e.write(e1); if(r1!=r2){r2>r1 ? r1++:r1--;} r.write(r1); 
         if(t1!=t2){t2>t1 ? t1++:t1--;} t.write(t1); if(y1!=y2){y2>y1 ? y1++:y1--;} y.write(y1); 
         if(u1!=u2){u2>u1 ? u1++:u1--;} u.write(u1); if(i1!=i2){i2>i1 ? i1++:i1--;} i.write(i1);
-        for(int k=0;k<ds;k++){espweb.handleClient();delayMicroseconds(100);} } }  
+        jiange(ds); } }  
 void loop(){espweb.handleClient();delay(10);ArduinoOTA.handle(); delay(10);  dnsServer.processNextRequest();delay(10);}
 void chongzhi(){if(digitalRead(D3)==0){setwifi(); delay(999); ESP.reset();}}
 void setwifi(){espwifi.resetSettings(); DynamicJsonDocument doc(50);file=SPIFFS.open("/pass.json","w");doc["pass"]=espweb.arg("setpass");
